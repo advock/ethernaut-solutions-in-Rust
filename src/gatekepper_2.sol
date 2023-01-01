4ed984e4db1c193e8c060d51997e2a3bb0986828
ADVK <advaiyakarnik@gmail.com>
2023-01-01 23:41:15 +0530

    denial hack

--- src/gatekepper_2.sol
+++ src/gatekepper_2.sol
@@ -1,3 +1,6 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8;
+
 contract Bro {
     constructor(address _ad) {
         bytes8 key = bytes8(
--- src/preservation.sol
+++ src/preservation.sol
@@ -1,3 +1,6 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8;
+
 contract Aack {
     address public timeZone1Library;
     address public timeZone2Library;
